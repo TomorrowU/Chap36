4e0b30f0879bdc4e10e977cfaa902a7d050cc128
TomorrowU <dbdhrcjs2@gmail.com>
2023-05-25 17:30:10 +0900

    chap36

--- chap36/src/main/java/com/example/mapper/DeptMapper.java
+++ chap36/src/main/java/com/example/mapper/DeptMapper.java
@@ -2,10 +2,12 @@ package com.example.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.example.model.Dept;
 
@@ -30,6 +32,37 @@ public interface DeptMapper {
 	int insert(@Param("deptno") int deptno, 
 			   @Param("dname")  String dname, 
 			   @Param("loc")	String loc);
+	
+	@Insert("""
+			insert into dept
+			values (
+			#{d.deptno}, 
+			#{d.dname, jdbcType=VARCHAR}, 
+			#{d.loc, jdbcType=VARCHAR}
+			)
+			""")
+	int insertDept(@Param("d") Dept dept);
+	
+	@Update("""
+			update dept
+				set dname = #{dname,  jdbcType=VARCHAR},
+				 	loc	  = #{loc, 	  jdbcType=VARCHAR}
+			where deptno  = #{deptno}	
+			""")
+	int update(@Param("deptno")int deptno,@Param("dname")String dname,@Param("loc")String loc);
 
+	@Update("""
+			update dept
+			set dname =  #{d.dname,jdbcType=VARCHAR},
+			 	loc	  =  #{d.loc,jdbcType=VARCHAR}
+			where deptno=#{d.deptno}
+			""")
+	int updateDept(@Param("d") Dept dept);
+	
+	@Delete("""
+			delete dept where deptno=#{deptno}
+			""")
+	int delete(@Param("deptno")int deptno);
+	
 }
  
--- chap36/src/test/java/com/example/mapper/DeptMapperTest.java
+++ chap36/src/test/java/com/example/mapper/DeptMapperTest.java
@@ -2,15 +2,18 @@ package com.example.mapper;
 
 import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.UncategorizedSQLException;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.model.Dept;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
@@ -56,6 +59,8 @@ public class DeptMapperTest {
 			deptMapper.insert(50, "개발3부", "서울");
 		} catch (DuplicateKeyException e) {
 			System.out.println("50번 부서는 사용할 수 없습니다.");
+			deptMapper.insert(51, "개발3부", "서울");
+			System.out.println(deptMapper.selectByDeptno(51));
 		}
 		
 		try {
@@ -64,10 +69,109 @@ public class DeptMapperTest {
 			System.out.println(e.getMessage());
 		}
 		
+		//DataIntegrityViolationException이 발생하면 성공
+		assertThrows(DataIntegrityViolationException.class,()->{
+			deptMapper.insert(100,"총무부", null);
+		});
+		
+		
 		
 		
 		
 	}
 	
-
+	@Test
+	@Transactional
+	void insertDept() throws IOException {
+		var dept = new Dept(50,"개발1부","서울");
+		int cnt = deptMapper.insertDept(dept);
+		assertThat(cnt).isEqualTo(1);
+		
+		dept= new Dept(60,"개발2부","null");
+		cnt =deptMapper.insertDept(dept);
+		assertThat(cnt).isEqualTo(1);
+		
+		assertThrows(DuplicateKeyException.class,()->{
+			deptMapper.insertDept(new Dept(60,"개발3부",null));
+		});
+		
+		assertThrows(DataIntegrityViolationException.class,()->{
+			deptMapper.insertDept(new Dept(200,"개발4부",null));
+		});
+		
+		objectMapper.createGenerator(System.out)
+					.useDefaultPrettyPrinter()
+					.writeObject(deptMapper.selectAll());
+		
+	}
+	
+	@Test
+	@Transactional
+	void update() throws IOException {
+		int cnt = deptMapper.update(10,"xxx","yyy");
+		assertThat(cnt).isEqualTo(1);
+		
+		cnt = deptMapper.update(60,"xxx","yyy");
+		assertThat(cnt).isEqualTo(0); //0이면 안된다고 확신 1이면 된다고 확신
+		
+		assertThrows(DataIntegrityViolationException.class, () -> {
+			try {
+				int c = deptMapper.update(20, null, null);
+			} catch (UncategorizedSQLException e) {
+				throw new DataIntegrityViolationException(e.getMessage());
+			}
+		});
+		
+		cnt=deptMapper.update(100,"개발4부" ,"부산");
+		assertThat(cnt).isEqualTo(0);
+		
+		cnt=deptMapper.update(30,"개발5부", null);
+		assertThat(cnt).isEqualTo(1);
+		
+		objectMapper.createGenerator(System.out)
+					.useDefaultPrettyPrinter()
+					.writeObject(deptMapper.selectAll());
+		
+	}
+	
+	@Test
+	@Transactional
+	void updateDept() throws IOException {
+		var dept = new Dept(10,"xxx","yyy");
+		int cnt = deptMapper.updateDept(dept);
+		assertThat(cnt).isEqualTo(1);
+		
+		dept = new Dept(60,"zzz","aaa");
+		cnt=deptMapper.updateDept(dept);
+		assertThat(cnt).isEqualTo(0);
+		
+		objectMapper.createGenerator(System.out)
+					.useDefaultPrettyPrinter()
+					.writeObject(deptMapper.selectAll());
+		
+	}
+	@Test
+	@Transactional
+	void delete() throws IOException {
+		int cnt = deptMapper.delete(90);
+		assertThat(cnt).isEqualTo(0);
+	
+		cnt = deptMapper.delete(40);
+		assertThat(cnt).isEqualTo(1);
+		
+		objectMapper.createGenerator(System.out)
+		.useDefaultPrettyPrinter()
+		.writeObject(deptMapper.selectAll());
+		
+		assertThrows(DataIntegrityViolationException.class,()->{
+			deptMapper.delete(10);
+		});
+		
+		
+		
+		
+	
+	}
+	
+	
 }
